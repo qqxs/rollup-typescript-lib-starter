d4bdb732e61ca1828386957a12f63f9e248b0973
freeshineit <xiaoshaoqq@gmail.com>
2021-01-10 14:52:33 +0800

    feat: 🎸 new esmodule example

---
+++ example/esmodule/index.ts
@@ -0,0 +1,7 @@
+import rolluptypescriptlibstarter from 'rollup-typescript-lib-starter'
+
+const person = new rolluptypescriptlibstarter('shine')
+console.log(person)
+console.log(person.getName())
+person.setName('shineshao')
+console.log(person.getName())

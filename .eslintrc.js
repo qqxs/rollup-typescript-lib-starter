a379b9f723ac8444afcb40b5535a2d4ddd24851e
ShineShao <xiaoshaoqq@gmail.com>
2021-04-10 22:07:29 +0800

    function-return-type

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     node: true
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': ['off']
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/explicit-function-return-type': 'off'
   }
 }

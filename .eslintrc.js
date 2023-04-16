b3952a22a0e0ea9514d2f7c3b357e1f89cfc92fc
ShineShao <xiaoshaoqq@gmail.com>
2023-04-16 16:12:33 +0800

    ci: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
-  plugins: ['@typescript-eslint'],
-  env: {
-    //指定代码的运行环境
-    browser: true,
-    node: true
+  extends: 'xx',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': ['off'],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/member-delimiter-style': 'off'
-  }
-}
+    // '@typescript-eslint/no-explicit-any': ['off'],
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/member-delimiter-style': 'off'
+  },
+};

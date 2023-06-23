85eaff849365980ad50ad0a5e6d1dbbe2d68156d
sankong <xiaoshaoqq@gmail.com>
2023-06-23 23:23:25 +0800

    ci: update

--- config/rollup.config.ts
+++ config/rollup.config.ts
@@ -14,7 +14,7 @@ const input = 'src/index.ts';
 const banner = `/*
 *
 * ${name}.js v${pkg.version}
-* (c) ${new Date().getFullYear()} ${pkg.author}
+* (c) ${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()} ${pkg.author}
 * Released under the MIT License.
 *
 */`;

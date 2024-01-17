bb2aa5d3f7e210f7e4e51e8fbe862442134f2634
ShineShao <xiaoshaoqq@gmail.com>
2024-01-17 23:56:11 +0800

    docs(ci): update

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   entryPoints: ['src/index.ts'],
   out: 'docs',
+  plugin: ['typedoc-plugin-rename-defaults'],
   exclude: ['node_modules'],
   includeVersion: true,
   hideGenerator: true,
-  disableSources: true,
+  disableSources: false,
   tsconfig: 'tsconfig.json',
   readme: 'README.md',
 };
